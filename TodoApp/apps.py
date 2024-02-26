816493969cc629a59fe1e9a49b035c2f84d9ab5b
Hareesh1783 <160411342+Hareesh1783@users.noreply.github.com>
2024-02-26 10:44:23 +0530

    Added a comment

--- TodoApp/apps.py
+++ TodoApp/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
+#if you want to create a app you should configure here
 
 class TodoappConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
